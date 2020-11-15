be5a7584abd01326be19eeb6ad293bc9cfaac346
ikcilrep <frajerzycki@protonmail.com>
2020-11-15 20:14:54 +0100

    Fetch data from cloud database

--- functions/books.js
+++ functions/books.js
@@ -1,8 +1,26 @@
-const books = require("../data/books.json");
+const { createApolloFetch } = require("apollo-fetch");
 
 exports.handler = async () => {
+  const fetch = createApolloFetch({
+    uri: "https://exotic-treefrog-16.hasura.app/v1/graphql",
+  });
+
+  const response = await fetch({
+    query: `{
+    book {
+      id
+      title
+      subtitle
+      cover
+      authors {
+        name
+      }
+    }
+  }`,
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify(books),
+    body: JSON.stringify(response.data.book),
   };
 };
